8bc0d68515256deb73a55aaf75e52f3615dcef80
Arvin Non <83747422+arvinnon@users.noreply.github.com>
2023-01-13 23:36:53 +0800

    Add files via upload

---
+++ myLogin/forgot.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Admission Portal</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Login Form" />
+    <!-- //Meta tag Keywords -->
+
+    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+    <!--/Style-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!--//Style-CSS -->
+
+    <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <!-- form section start -->
+    <section class="w3l-mockup-form" >
+        <div class="container">
+            <!-- /form -->
+            <div class="workinghny-form-grid">
+                <div class="main-mockup">
+                   
+                    <div class="w3l_form align-self">
+                        <div class="left_grid_info">
+                            <img src="images/hau-logo.png" alt="">
+                        </div>
+                    </div>
+                    <div class="content-wthree">
+                        <h2>Forgot Password</h2>
+                        
+                        <form action="" method="post">
+                            <input type="email" class="email" name="email" placeholder="Enter Your Email" required>
+                            <button name="submit" class="btn" type="submit">Send Reset Link</button>
+                        </form>
+                        <div class="social-icons">
+                            <p>Back to! <a href="index.php">Login</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //form -->
+        </div>
+    </section>
+    <!-- //form section start -->
+
+    <script src="js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function (c) {
+            $('.alert-close').on('click', function (c) {
+                $('.main-mockup').fadeOut('slow', function (c) {
+                    $('.main-mockup').remove();
+                });
+            });
+        });
+    </script>
+
+</body>
+
+</html>
